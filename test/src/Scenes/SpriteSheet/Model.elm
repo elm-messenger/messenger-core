471b60c9a569e20db2b949f91cd518b0fd5eea5e
linsyking <kxiang@umich.edu>
2025-07-09 11:25:10 +0800

    feat: spritesheet

--- test/src/Scenes/SpriteSheet/Model.elm
+++ test/src/Scenes/SpriteSheet/Model.elm
@@ -8,9 +8,11 @@ module Scenes.SpriteSheet.Model exposing (scene)
 
 import Lib.Base exposing (SceneMsg)
 import Lib.UserData exposing (UserData)
+import Messenger.Render.Texture exposing (renderSprite)
 import Messenger.Scene.RawScene exposing (RawSceneInit, RawSceneUpdate, RawSceneView, genRawScene)
 import Messenger.Scene.Scene exposing (MConcreteScene, SceneStorage)
 import REGL.BuiltinPrograms as P
+import REGL.Common exposing (group)
 
 
 type alias Data =
@@ -29,7 +31,29 @@ update env msg data =
 
 view : RawSceneView UserData Data
 view env data =
-    P.empty
+    let
+        gd =
+            env.globalData
+
+        id =
+            gd.internalData
+
+        rate =
+            100
+
+        currentAct x =
+            String.fromInt (modBy x (floor (gd.sceneStartTime / rate)))
+    in
+    group []
+        [ renderSprite id ( 100, 300 ) ( 100, 0 ) ("char0" ++ currentAct 13)
+        , renderSprite id ( 300, 300 ) ( 100, 0 ) ("char1" ++ currentAct 8)
+        , renderSprite id ( 500, 300 ) ( 100, 0 ) ("char2" ++ currentAct 10)
+        , renderSprite id ( 700, 300 ) ( 100, 0 ) ("char3" ++ currentAct 10)
+        , renderSprite id ( 900, 300 ) ( 100, 0 ) ("char4" ++ currentAct 10)
+        , renderSprite id ( 1100, 300 ) ( 100, 0 ) ("char5" ++ currentAct 6)
+        , renderSprite id ( 1300, 300 ) ( 100, 0 ) ("char6" ++ currentAct 4)
+        , renderSprite id ( 1500, 300 ) ( 100, 0 ) ("char7" ++ currentAct 7)
+        ]
 
 
 scenecon : MConcreteScene Data UserData SceneMsg
